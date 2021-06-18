f5cd5b0fa0ec43526c66ee7ab8402d3e5fefec09
Hasib <67379993+hasib231@users.noreply.github.com>
2021-06-18 20:12:42 +0600

    Add files via upload

---
+++ page 21.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char small,capital;
+    small=getchar();
+    capital=small-32;
+    printf("capital letter is:%c\n",capital);
+
+    return 0;
+}
